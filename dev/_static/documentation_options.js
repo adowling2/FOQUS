af39ea400cd7a587028af1e0b6fa621daab47b3d
Keith Beattie <KSBeattie@lbl.gov>
2019-03-06 01:11:52 +0000

    Updating dev docs

--- dev/_static/documentation_options.js
+++ dev/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2.5.0.dev187+g288fa13',
+    VERSION: '&#34;2.6.0.rc1.dev28+gb945eb6&#34;',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
